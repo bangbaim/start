3787a966f812297adcfe67b8f162b542b9e62340
bangbaim <85072227+bangbaim@users.noreply.github.com>
2021-06-01 07:10:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 mv hellminer A
-./A -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMbFHPn1V7fqCSpDVTmYRPBAWPpxghnrQD.Baim -p x --cpu 34
+./A -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMbFHPn1V7fqCSpDVTmYRPBAWPpxghnrQD.Baim -p x --cpu 2
